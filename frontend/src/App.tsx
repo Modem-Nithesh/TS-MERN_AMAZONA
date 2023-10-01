7631a037b27bcfdeb4668b7a27977d490028c037
Modem Nithesh <modnithesh@gmail.com>
2023-10-01 15:42:14 +0530

    List-products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,21 @@ function App() {
     <div>
       <header>TS Amazona</header>
 
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All Rights Reserved</footer>
     </div>
   );
