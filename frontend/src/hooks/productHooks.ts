051ce700a06e87b9762bfb0595564bc50ae0ebf2
Modem Nithesh <modnithesh@gmail.com>
2023-10-05 10:07:23 +0530

    add Product page

--- frontend/src/hooks/productHooks.ts
+++ frontend/src/hooks/productHooks.ts
@@ -7,3 +7,11 @@ export const useGetProductsQuery = () => useQuery(
         queryKey: ['products'],
         queryFn: async () => ( await apiClient.get<Product[]>(`api/products`)).data
     })
+
+
+    export const useGetProductDetailsBySlugQuery = (slug: string) =>
+    useQuery({
+      queryKey: ['products', slug],
+      queryFn: async () => (await apiClient.get<Product>(`api/products/${slug}`)).data
+    })
+     
