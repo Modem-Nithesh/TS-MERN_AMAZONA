c00406834832aea6a11eeb7b0df230cdf145265a
Modem Nithesh <modnithesh@gmail.com>
2023-10-05 17:24:16 +0530

    Add to Card functionality

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -2,8 +2,30 @@ import { Button, Card, CardBody } from "react-bootstrap";
 import { Product } from "../types/Product";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
+import { useContext } from "react";
+import { Store } from "../Store";
+import { CartItem } from "../types/Cart";
+import { convertProductToCartItem } from "../utils";
 
 function ProductItem({ product }: { product: Product }) {
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = (item: CartItem) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -17,9 +39,18 @@ function ProductItem({ product }: { product: Product }) {
 
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-         
-        {product.countInStock === 0 ? <Button variant="light" disabled>Out of Stock</Button>: <Button>Add to Cart</Button>}
 
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of Stock
+          </Button>
+        ) : (
+          <Button
+            onClick={() => addToCartHandler(convertProductToCartItem(product))}
+          >
+            Add to Cart
+          </Button>
+        )}
       </CardBody>
     </Card>
   );
