f075c21f865b79155f27c710a4b0ea9f5a741060
Modem Nithesh <modnithesh@gmail.com>
2023-10-10 09:51:04 +0530

    Sign In Page

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Cart, CartItem } from "./types/Cart";
+import { UserInfo } from "./types/UserInfo";
 
 type AppState = {
   mode: string;
   cart: Cart;
+  userInfo?: UserInfo;
 };
 
 const initialState: AppState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo")!)
+    : null,
+
   mode: localStorage.getItem("mode")
     ? localStorage.getItem("mode")!
     : window.matchMedia &&
@@ -33,7 +39,9 @@ const initialState: AppState = {
 type Action =
   | { type: "SWITCH_MODE" }
   | { type: "CART_ADD_ITEM"; payload: CartItem }
-  | { type: "CART_REMOVE_ITEM"; payload: CartItem };
+  | { type: "CART_REMOVE_ITEM"; payload: CartItem }
+  | { type: "USER_SIGNIN"; payload: UserInfo }
+  | { type: "USER_SIGNOUT" };
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
@@ -55,14 +63,43 @@ function reducer(state: AppState, action: Action): AppState {
 
       return { ...state, cart: { ...state.cart, cartItems } };
 
-      case 'CART_REMOVE_ITEM': {
-        const cartItems = state.cart.cartItems.filter(
-          (item: CartItem) => item._id !== action.payload._id
-        )
-        localStorage.setItem('cartItems', JSON.stringify(cartItems))
-        return { ...state, cart: { ...state.cart, cartItems } }
-      }
-      
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item: CartItem) => item._id !== action.payload._id
+      );
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+
+    case "USER_SIGNIN":
+      return { ...state, userInfo: action.payload };
+
+    case "USER_SIGNIN":
+      return { ...state, userInfo: action.payload };
+    case "USER_SIGNOUT":
+      return {
+        mode:
+          window.matchMedia &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light",
+        cart: {
+          cartItems: [],
+          paymentMethod: "PayPal",
+          shippingAddress: {
+            fullName: "",
+            address: "",
+            postalCode: "",
+            city: "",
+            country: "",
+          },
+          itemsPrice: 0,
+          shippingPrice: 0,
+          taxPrice: 0,
+          totalPrice: 0,
+        },
+      };
+
     default:
       return state;
   }
